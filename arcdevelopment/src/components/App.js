fe70464982d3fc86daaeaa42eb237f197834f766
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-20 11:01:06 -0500

    header rendering on dom

--- arcdevelopment/src/components/App.js
+++ arcdevelopment/src/components/App.js
@@ -1,11 +1,14 @@
 
 import Button from '@material-ui/core/Button'
+import Header from './ui/header'
 
 
 function App() {
   return (
     <div className="App">
-      Hola!
+      <Header >
+
+      </Header>
     </div>
   );
 }
