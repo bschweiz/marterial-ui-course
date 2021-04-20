edb4a82187f91d8226cfc43ceed5e248f045fd33
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-20 11:13:30 -0500

    trying to apply some lorem

--- arcdevelopment/src/components/App.js
+++ arcdevelopment/src/components/App.js
@@ -1,5 +1,4 @@
-
-import Button from '@material-ui/core/Button'
+import React from 'react';
 import Header from './ui/header'
 
 
@@ -7,7 +6,29 @@ function App() {
   return (
     <div className="App">
       <Header >
+      {[...new Array(12)]
+                        .map(
+                            () => `You stay on top of her, Buddy. Don't be afraid to ride her. Hard. You're Killing Me, Buster. In the mid '90s, Tobias formed a folk music band with Lindsay and Maebe which he called Dr. Funke's 100 Percent Natural Good Time Family Band Solution. The group was underwritten by the Natural Food Life Company, a division of Chem-Grow, an Allen Crayne acqusition, which was part of the Squimm Group. Their motto was simple: We keep you alive. I am getting rid of this thing. It has caused me nothing but pride and self-respect. I don't appreciate the dry British humor.
 
+Boy, I sure feel like a Mary without a Peter and a Paul. One of the guys told me to take my head out of my BOTTOM and get back to work…my BOTTOM! I've always been deeply passionate about nature. Perhaps you remember Neuterfest? I'll never forget your wedding.
+
+Come on, this is a Bluth family celebration. It's no place for children. She's a contestant. It's sorta like an inner beauty pageant. Ah, there it is. That's how Tony Wonder lost a nut. That coat costs more than your house! What's up, fizz-ellas. We need a name. Maybe "Operation Hot Mother." No, let's try to top that. (They never did.) No, I was ashamed to be SEEN with you. I like being with you. I'll buy you a hundred George Michaels that you can teach to drive!
+
+Mission Accomplished. No, she's in it. She's a contestant. It's sorta like an inner beauty pageant. Ah, there it is. Dead Dove DO NOT EAT. Chicken fingers… with spicy club sauce.
+
+Look, you are playing adults…with fully formed libidos, not 2 young men playing grab-ass in the shower. Ah, it is a rock, though. Should beat everything. Gob: There's not a lot of logic to it.
+
+She wanted to look 48. I nearly airbrushed her into oblivion. Ended up checking "albino" on the form. Who? i just dont want him to point out my cracker ass in front of ann. Her lawyers are claiming the seal is worth $250,000. And that's not even including Buster's Swatch. Do the right thing here. String this blind girl along so that dad doesn't have to pay his debt to society. I'm sure, wherever your father is, she loves you very much.
+
+You might wanna lean away from that fire since you're soaked in alcohol. Chickens don't clap!
+
+Well excuse me for liking the way they frame my junk! I'll buy you a hundred George Michaels that you can teach to drive! A-coodle-doodle-doo. A-coodle-doodle-doo. What, so the guy we are meeting with can't even grow his own hair? COME ON! Sorry, some of my students are arguing the significance of the shankbone on the seder plate. But we do not - NOT wag our genitals at one another to make a point. Hey, look at that – you're mean sober, too.
+
+You stay on top of her, Buddy. Don't be afraid to ride her. Hard. And although the intervention didn't work, it turned into one of the Bluth family's better parties. He also said some things African American-y wasn't ready to hear. Buster's in what we like to call a light to no coma. In laymans terms, it might be considered a very heavy nap. You're losing blood, aren't you? Gob: Probably, my socks are wet.
+
+I made a huge tiny mistake. No, she's in it. She's a contestant. It's sorta like an inner beauty pageant. Ah, there it is. The only thing more terrifying than the escaped lunatic's hook was his twisted call… Hey campers! What do you expect, Mother? I'm half machine! I'm a monster!! Uncle Gob… was Aunt Lindsay ever pregnant? Yeah, sure, dozens of times. I hear the jury's still out on science.`,
+                        )
+                        .join('\n')}
       </Header>
     </div>
   );
