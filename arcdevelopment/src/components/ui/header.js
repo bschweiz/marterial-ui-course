3379df92da0b8f8383b1ac6b0aa177ae3edee173
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-20 15:06:12 -0500

    proper active page state on refresh of each view

--- arcdevelopment/src/components/ui/header.js
+++ arcdevelopment/src/components/ui/header.js
@@ -57,10 +57,21 @@ export default function Header(props) {
 
     useEffect(() => {
         if (window.location.pathname === "/" && value !== 0) {
-            setValue(0)
+            setValue(0);
+        } else if (window.location.pathname === "/photos" && value !==
+        1) {
+            setValue(1);
+        } else if (window.location.pathname === "/videos" && value !==
+        2) {
+            setValue(2);
+        } else if (window.location.pathname === "/drawings" && value !==
+        3) {
+            setValue(3);
+        } else if (window.location.pathname === "/contact" && value !==
+        4) {
+            setValue(4);
         }
-
-    })
+    }, [value])
     return (
         <React.Fragment>
 
