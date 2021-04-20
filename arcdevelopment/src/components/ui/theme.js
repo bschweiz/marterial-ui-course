c707e558a5af55784f2502cfe09c16d9b23c345f
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-20 12:00:27 -0500

    testing custom palette

--- arcdevelopment/src/components/ui/theme.js
+++ arcdevelopment/src/components/ui/theme.js
@@ -2,13 +2,22 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 
+const bBlue = "#264D59"
+const bTeal = "#43978D"
+
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: purple[500],
+    palette: {
+        common: {
+            blue: `${bBlue}`,
+            teal: `${bTeal}`
+        },
+        primary: {
+            main: `${bBlue}`,
+        },
+        secondary: {
+            main: `${bTeal}`,
+        },
     },
-    secondary: {
-      main: green[500],
-    },
-  },
 });
+
+
