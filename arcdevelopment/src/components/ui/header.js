7cf952f4c87df08b47dc921cc62f16dfa851f433
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-20 11:06:17 -0500

    adding elevated scoll ui touch

--- arcdevelopment/src/components/ui/header.js
+++ arcdevelopment/src/components/ui/header.js
@@ -1,13 +1,69 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
 
 export default function Header(props) {
     return (
         <AppBar position="fixed" >
             <Toolbar >
-                BSCHWEIZ 
+                BSCHWEIZ
             </Toolbar>
         </AppBar>
     )
+}
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+
+
+
+export default function ElevateAppBar(props) {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <ElevationScroll {...props}>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6">Scroll to Elevate App Bar</Typography>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <Toolbar />
+            <Container>
+                <Box my={2}>
+                    {[...new Array(12)]
+                        .map(
+                            () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+                        )
+                        .join('\n')}
+                </Box>
+            </Container>
+        </React.Fragment>
+    );
 }
